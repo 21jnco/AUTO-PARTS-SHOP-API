b6b97b9865347bc23a55b19b03e85e3a652778bb
Kostiantyn <kostiaolnev@gmail.com>
2026-05-23 21:26:16 -0700

    chore: add database configuration

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from app.config import DATABASE_URL
+
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+class Base(DeclarativeBase):
+    pass
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False
+    )
+
+def get_db():
+    db = SessionLocal
+
+    try:
+        yield db
+
+    finally:
+        db.close()
