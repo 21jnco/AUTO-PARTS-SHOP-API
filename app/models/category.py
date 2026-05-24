7a270d71b9e8488b33e3242c4541677e6bc545ff
Kostiantyn <kostiaolnev@gmail.com>
2026-05-23 21:27:34 -0700

    feat: add category model

---
+++ app/models/category.py
@@ -0,0 +1,48 @@
+from app.database import Base
+
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import (
+    Integer, 
+    Boolean, 
+    String,
+    Text,
+    DateTime
+    )
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True)
+
+    name: Mapped[str] = mapped_column(
+        String(255),
+        unique=True,
+        nullable=False,
+        index=True)
+
+    description: Mapped[str] = mapped_column(
+        Text,
+        nullable=False)
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        nullable=False
+        )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
