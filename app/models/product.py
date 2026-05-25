fb59cde09c3e545a4da54ff77c78bdae3b3d42ac
Kostiantyn <kostiaolnev@gmail.com>
2026-05-24 17:47:13 -0700

    feat: add product model

---
+++ app/models/product.py
@@ -0,0 +1,72 @@
+from app.database import Base
+
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import (
+    ForeignKey,
+    Integer,
+    Numeric,
+    Boolean,
+    DateTime,
+    Text,
+    String)
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True)
+
+    category_id: Mapped[int] = mapped_column(
+        ForeignKey("categories.id"),
+        nullable=False
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(255),
+        index=True,
+        nullable=False
+    )
+
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    price: Mapped[Decimal] = mapped_column(
+        Numeric(10, 2),
+        nullable=False
+    )
+
+    stock_quantity: Mapped[int] = mapped_column(
+        Integer,
+        default=0,
+        nullable=False
+    )
+
+    is_active: Mapped[bool] = mapped_column(
+        Boolean,
+        default=True,
+        nullable=False
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        nullable=False
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
+
+    category = relationship(
+        "Category",
+        back_populates="products"
+    )
